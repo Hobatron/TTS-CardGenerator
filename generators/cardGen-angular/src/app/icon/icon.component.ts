20c3b4e1983e98a485848b2bea26f752a62dbaf9
Mason <masonashlock@gmail.com>
2023-07-11 23:43:48 -0500

    updated all csvs, updated slot display

--- generators/cardGen-angular/src/app/icon/icon.component.ts
+++ generators/cardGen-angular/src/app/icon/icon.component.ts
@@ -2,109 +2,109 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Icon } from '../services/csv.service';
 
 interface CharColor {
-  char?: string;
-  color?: string;
+	char?: string;
+	color?: string;
 }
 
 @Component({
-  selector: 'app-icon',
-  templateUrl: './icon.component.html',
-  styleUrls: ['./icon.component.scss'],
+	selector: 'app-icon',
+	templateUrl: './icon.component.html',
+	styleUrls: ['./icon.component.scss'],
 })
 //#FB3640 - Red
 //#9BC53D - Green
 //#00A1E4 - Blue
 //#E1BC29 - Gold
 export class IconComponent implements OnInit {
-  @Input() icon?: Icon;
-  @Input() fontSize: number = 48;
-  public iconChars: Array<CharColor> = new Array();
-  public displayValue = '';
-  constructor() {}
+	@Input() icon?: Icon;
+	@Input() fontSize: number = 48;
+	public iconChars: Array<CharColor> = new Array();
+	public displayValue = '';
+	constructor() {}
 
-  ngOnInit(): void {
-    switch (this.icon?.type.trim()) {
-      case 'gem': {
-        this.setGems(this.icon.value as string);
-        break;
-      }
-      case 'gold': {
-        this.iconChars.push({
-          char: 'B',
-          color: '#E1BC29',
-        });
-        this.displayValue = this.icon?.value + 'x';
-        break;
-      }
-      case 'slot': {
-        this.displayValue = this.icon?.value + '+';
-        break;
-      }
-      case 'dice': {
-        this.setDiceSymbol(this.icon.value as string);
-        break;
-      }
-    }
-  }
+	ngOnInit(): void {
+		switch (this.icon?.type.trim()) {
+			case 'gem': {
+				this.setGems(this.icon.value as string);
+				break;
+			}
+			case 'gold': {
+				this.iconChars.push({
+					char: 'B',
+					color: '#E1BC29',
+				});
+				this.displayValue = this.icon?.value + ' ';
+				break;
+			}
+			case 'slot level': {
+				this.displayValue = 'slot level' + this.icon?.value;
+				break;
+			}
+			case 'dice': {
+				this.setDiceSymbol(this.icon.value as string);
+				break;
+			}
+		}
+	}
 
-  private setDiceSymbol(faces: string) {
-    faces.split('-').forEach((face) => {
-      switch (face) {
-        case 'Crit': {
-          this.iconChars.push({
-            char: 'C',
-            color: '#000',
-          });
-          break;
-        }
-        case 'Hit': {
-          this.iconChars.push({
-            char: 'H',
-            color: '#000',
-          });
-          break;
-        }
-        case '!': {
-          this.iconChars.push({
-            char: '!',
-            color: '#E1BC29',
-          });
-          break;
-        }
-        case 'Miss': {
-          this.iconChars.push({
-            char: 'M',
-            color: '#FB3640',
-          });
-          break;
-        }
-      }
-    });
-  }
+	private setDiceSymbol(faces: string) {
+		faces.split('-').forEach((face) => {
+			switch (face) {
+				case 'Crit': {
+					this.iconChars.push({
+						char: 'C',
+						color: '#000',
+					});
+					break;
+				}
+				case 'Hit': {
+					this.iconChars.push({
+						char: 'H',
+						color: '#000',
+					});
+					break;
+				}
+				case '!': {
+					this.iconChars.push({
+						char: '!',
+						color: '#E1BC29',
+					});
+					break;
+				}
+				case 'Miss': {
+					this.iconChars.push({
+						char: 'M',
+						color: '#FB3640',
+					});
+					break;
+				}
+			}
+		});
+	}
 
-  private setGems(value: string): void {
-    //Format for value: G,G-R-B
-    value.split('-').forEach((gem) => {
-      let color = '';
-      switch (gem) {
-        case 'R': {
-          color = '#FB3640';
-          break;
-        }
-        case 'G': {
-          color = '#9BC53D';
-          break;
-        }
-        case 'B': {
-          color = '#00A1E4';
-          break;
-        }
-      }
+	private setGems(value: string): void {
+		//Format for value: G,G-R-B
+		value.split('-').forEach((gem) => {
+			let color = '';
+			switch (gem) {
+				case 'R': {
+					color = '#FB3640';
+					break;
+				}
+				case 'G': {
+					color = '#9BC53D';
+					break;
+				}
+				case 'B': {
+					color = '#00A1E4';
+					break;
+				}
+			}
 
-      this.iconChars.push({
-        char: 'G',
-        color,
-      });
-    });
-  }
+			this.iconChars.push({
+				char: 'G',
+				color,
+			});
+		});
+	}
 }
--- generators/cardGen-angular/src/app/mappers/mapper.ts
+++ generators/cardGen-angular/src/app/mappers/mapper.ts
@@ -1,22 +1,24 @@
-import { Icon } from "../services/csv.service";
+import { Icon } from '../services/csv.service';
 
 export class Mapper {
-    cost(col: string): Icon[] {
-        let cost: Icon[] = [] as Icon[];
-        if (!col) {
-          return [];
-        }
-        col = col.replace(/[{}]/g, '');
-        let keyValues: Array<string[]> = [];
-        col.split(';').forEach(keyValue => keyValues.push(keyValue.split(':')));
-        keyValues.reverse().forEach(kv => { //.reverse() since they float right, this keeps them in order typed
-          cost.push({
-            type: kv[0].toLowerCase() as 'gem' | 'gold' | 'slot',
-            value: kv[1]
-          });
-        });
-    
-        return cost;
-      }
-}
+	cost(col: string): Icon[] {
+		let cost: Icon[] = [] as Icon[];
+		if (!col) {
+			return [];
+		}
+		col = col.replace(/[{}]/g, '');
+		let keyValues: Array<string[]> = [];
+		col.split(';').forEach((keyValue) =>
+			keyValues.push(keyValue.split(':'))
+		);
+		keyValues.reverse().forEach((kv) => {
+			//.reverse() since they float right, this keeps them in order typed
+			cost.push({
+				type: kv[0].toLowerCase() as 'gem' | 'gold' | 'slot level',
+				value: kv[1],
+			});
+		});
 
+		return cost;
+	}
+}
